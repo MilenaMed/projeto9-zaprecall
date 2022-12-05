14bc8dce10502fb44ffb85f25683bfc57f08e27e
MilenaMed <milenamfm@gmail.com>
2022-12-05 15:45:55 -0300

    Mudando as perguntas e respostas

--- src/componentes/cards.js
+++ src/componentes/cards.js
@@ -1,14 +1,14 @@
 import setaPlay from '../img/seta_play.png'
 
 let cards = [
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "1", question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "2", question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "3", question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "4", question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "5", question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "6", question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "7", question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
-    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "8", question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "1", question: "O Dia depois do Fora: Qual o curso a Melissa faz?", answer: "Odontologia" },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "2", question: "O Jardim Secreto: Onde estava a chave para o jardim?", answer: "Num canteiro próximo ao muro do jardim" },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "3", question: "10 Minutos e 38 Segundos Neste Mundo Estranho: Onde os amigos 'enterram' Leila Tequila?", answer: "Na água" },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "4", question: "Os dois Morrem no Final: Como o Mateo morre?", answer: "Explosão na cozinha, quando vai fazer um chá" },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "5", question: "O Oceano no Fim do Caminho: Como a Velha senhora  Hempstock gosta da lua no outro lado da casa?", answer: "Cheia" },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "6", question: "Olhos d'água: O que acontece ao final da festa de aniversário de Ana Davenga?", answer: "Ao término de sua festa de aniversário, organizada pelo marido, ambos acabam assassinados em confronto com a polícia." },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "7", question: "Crônicas de uma Morte Anunciada: Porque Santiago Nasar levantou as 5h30 no fatidico dia?", answer: "No dia em que o matariam, Santiago Nasar levantou-se às 5h30m da manhã para esperar o navio em que chegava o bispo." },
+    { concluido: false, riscar: "", img: setaPlay, color: "#333333", aberto: false, number: "8", question: "Capitães da Areia: Onde moram os Capitães da Areia?", answer: "No trapiche" }
 ]
 
 export default cards
